89d133e7f149bc548d53c43b765548ed550731b2
Pawel Kondraciuk <kondraciukpawel@gmail.com>
2018-02-23 11:59:57 +0100

    Add SSR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({appId: 'my-app'})
   ],
   providers: [],
   bootstrap: [AppComponent]
