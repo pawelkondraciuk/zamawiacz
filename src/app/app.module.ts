18d58d33ab578a09c6eaba65ec76493d7df5d323
Pawel Kondraciuk <kondraciukpawel@gmail.com>
2018-03-07 19:53:22 +0100

    Add apollo client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { GraphQLModule } from './apollo.config';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'my-app'}),
-    HttpModule,
+    GraphQLModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
